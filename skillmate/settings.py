dbc03ef1dae16ffec189d5de9715e8f23242b584
joseyawovi <136805772+joseyawovi@users.noreply.github.com>
2025-03-30 19:23:03 +0000

    fixing

--- skillmate/settings.py
+++ skillmate/settings.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv(os.path.expanduser("~/.env")
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
